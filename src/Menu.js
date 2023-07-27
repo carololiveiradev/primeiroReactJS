bcf962eff4e6c2081b627d76420e08505e5248f1
Caroline Oliveira <devcarolineoliveira@gmail.com>
2023-07-26 21:19:25 -0300

    Criando menu

---
+++ src/Menu.js
@@ -0,0 +1,19 @@
+// src/Menu.js
+
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <nav>
+    <div className="menu">
+      <ul>
+        <li><a href="#">Página Inicial</a></li>
+        <li><a href="#">Produtos</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </div>
+    </nav>
+  );
+};
+
+export default Menu;
